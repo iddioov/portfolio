c00af0b0ec93600b2e77ce5f75a96df84dc045bb
iddioov <iddioov@gmail.com>
2026-03-31 14:13:39 +0900

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,35 @@
 "use strict";
+
+//프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector(".categories");
+const projects = document.querySelectorAll(".project");
+const projectContainer = document.querySelector(".projects");
+
+categories.addEventListener("click", (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const Active = document.querySelector(".category--selected");
+  Active.classList.remove("category--selected");
+  target.classList.add("category--selected");
+}
+
+function filterProjects(filter) {
+  projects.forEach((project) => {
+    if (filter === "all" || filter === project.dataset.type) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projectContainer.classList.remove("anim-out");
+  }, 250);
+}
