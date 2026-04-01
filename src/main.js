fae35a01450a2755396112a5b8cd63b5c5171c34
iddioov <iddioov@gmail.com>
2026-04-01 14:55:57 +0900

    Observer-part1

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@
 const header = document.querySelector(".header");
 const headerHeight = header.offsetHeight;
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
   if (window.scrollY > headerHeight) {
     header.classList.add("header--dark");
   } else {
