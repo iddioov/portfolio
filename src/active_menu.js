ec974892d81ae3e7bae6f495974dced7ce29c706
iddioov <iddioov@gmail.com>
2026-04-01 15:28:45 +0900

    Observer-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -19,8 +19,12 @@ const navItems = sectionIds.map((id) =>
   document.querySelector(`[href="${id}"`),
 );
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: "-20% 0px 0px 0px",
+  threshold: [0, 0.99],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -32,16 +36,24 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
-    console.log(visibleSections);
+      entry.intersectionRatio >= 0.95;
   });
 
   const navIndex = selectLastOne
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove("active");
+  activeNavItem = navItem;
+  activeNavItem.classList.add("active");
 }
